8bdf8a4c37628a973601ac998bad86426307a56d
Wemimo Ibidunmoye <53876543+wemiibidun@users.noreply.github.com>
2021-02-12 09:58:43 -0700

    Update variable_switch.py

--- Coding_Exercises_Udemy/variable_switch.py
+++ Coding_Exercises_Udemy/variable_switch.py
@@ -1,6 +1,6 @@
 '''
 Write a program that switches the values stored in the variables a and b. e.g:5,100, should print 100,5
-
+Example of output: https://cdn.fs.teachablecdn.com/tgdNl0iSqK6RpPyYZh9d
 '''
 ####################################
 #Write your code below this line 👇
