c6a15bfe09f441d5136a211ce4797a71d907f5e8
Wemimo Ibidunmoye <53876543+wemiibidun@users.noreply.github.com>
2021-02-12 09:09:13 -0700

    Update variable_switch.py

--- Coding_Exercises_Udemy/variable_switch.py
+++ Coding_Exercises_Udemy/variable_switch.py
@@ -1,12 +1,13 @@
 '''
-Write a program that switches the values stored in the variables a and b. 
+Write a program that switches the values stored in the variables a and b. e.g:5,100, should print 100,5
+
 '''
 ####################################
 #Write your code below this line 👇
 a,b = b,a
-#e.g:5,100 = 100,5
 
-#Python values are stored on the right
-#c=a
-#a=b
-#b=c
+#Another method. Since Python values are stored on the right
+
+c=a
+a=b
+b=c
