009208c3cc9b4fc0a005c31599ad86c96b90e8ab
Wemimo Ibidunmoye <53876543+wemiibidun@users.noreply.github.com>
2021-02-12 09:08:00 -0700

    Create variable_switch.py

---
+++ Coding_Exercises_Udemy/variable_switch.py
@@ -0,0 +1,12 @@
+'''
+Write a program that switches the values stored in the variables a and b. 
+'''
+####################################
+#Write your code below this line 👇
+a,b = b,a
+#e.g:5,100 = 100,5
+
+#Python values are stored on the right
+#c=a
+#a=b
+#b=c
