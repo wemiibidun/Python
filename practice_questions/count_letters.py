24620ff787cdf8975a3ce1f095c7b28814609c7b
Wemimo Ibidunmoye <53876543+wemiibidun@users.noreply.github.com>
2021-03-03 07:33:08 -0700

    Update count_letters.py

--- practice_questions/count_letters.py
+++ practice_questions/count_letters.py
@@ -1,9 +1,11 @@
+'''
 Write a function called unique_english_letters that takes the string word as a parameter. 
 The function should return the total number of unique letters in the string. 
 Uppercase and lowercase letters should be counted as different letters.
 We’ve given you a list of every uppercase and lower case letter in the English alphabet. It will be helpful to include that list in your function.
 
 letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" #Makes a string of the letters in alphabet to count
+'''
 
 # Write your unique_english_letters function here:
 def unique_english_letters(word):
@@ -14,8 +16,8 @@ def unique_english_letters(word):
   return counter
 
 
-# Uncomment these function calls to test your function:
-print(unique_english_letters("mississippi"))
-# should print 4
-print(unique_english_letters("Apple"))
-# should print 4
+#Uncomment these function calls to test your function:
+#print(unique_english_letters("mississippi"))
+#should print 4
+#print(unique_english_letters("Apple"))
+#should print 4
