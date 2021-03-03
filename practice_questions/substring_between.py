44488a1e9cd703e2c38ade051e5a8f3c30902071
Wemimo Ibidunmoye <53876543+wemiibidun@users.noreply.github.com>
2021-03-03 07:27:35 -0700

    Update substring_between.py

--- practice_questions/substring_between.py
+++ practice_questions/substring_between.py
@@ -1,8 +1,10 @@
+'''
 Write a function named substring_between_letters that takes a string named word, a single character named start, and another character named end. 
 This function should return the substring between the first occurrence of start and end in word. 
 If start or end are not in word, the function should return word.
 
 For example, substring_between_letters("apple", "p", "e") should return "pl".
+'''
 
 # Write your substring_between_letters function here:
 def substring_between_letters(word, start, end):
@@ -12,8 +14,8 @@ def substring_between_letters(word, start, end):
   	return(word[start_index+1:end_index])
   return word
 
-# Uncomment these function calls to test your tip function:
-print(substring_between_letters("apple", "p", "e"))
-# should print "pl"
-print(substring_between_letters("apple", "p", "c"))
-# should print "apple"
+#Uncomment these function calls to test your tip function:
+#print(substring_between_letters("apple", "p", "e"))
+#should print "pl"
+#print(substring_between_letters("apple", "p", "c"))
+#should print "apple"
