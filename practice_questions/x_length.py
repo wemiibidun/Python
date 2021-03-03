b6ffe408464ad087361f339ab569f0d2f356499c
wemiibidun <53876543+wemiibidun@users.noreply.github.com>
2021-03-03 07:23:32 -0700

    update

---
+++ practice_questions/x_length.py
@@ -0,0 +1,16 @@
+Create a function called x_length_words that takes a string named sentence and an integer named x as parameters. 
+This function should return True if every word in sentence has a length greater than or equal to x.
+
+# Write your x_length_words function here:
+def x_length_words(sentence,x):
+  words = sentence.split(" ")
+  for word in words:
+    if len(word) < x:
+      return False
+  return True 
+  
+# Uncomment these function calls to test your tip function:
+print(x_length_words("i like apples", 2))
+# should print False
+print(x_length_words("he likes apples", 2))
+# should print True
