8d04c789759eaab8d4acae729b0aaaff9d26b1c9
Wemimo Ibidunmoye <53876543+wemiibidun@users.noreply.github.com>
2021-03-03 07:31:41 -0700

    Update every_other_letter.py

--- practice_questions/every_other_letter.py
+++ practice_questions/every_other_letter.py
@@ -1,6 +1,7 @@
+'''
 Create a function named every_other_letter that takes a string named word as a parameter. 
 The function should return a string containing every other letter in word.
-
+'''
 
 # Write your every_other_letter function here:
 def every_other_letter(word):
@@ -10,10 +11,10 @@ def every_other_letter(word):
   return letter
 
 
-# Uncomment these function calls to test your function:
-print(every_other_letter("Codecademy"))
-# should print Cdcdm
-print(every_other_letter("Hello world!"))
-# should print Hlowrd
-print(every_other_letter(""))
-# should print 
+#Uncomment these function calls to test your function:
+#print(every_other_letter("Codecademy"))
+#should print Cdcdm
+#print(every_other_letter("Hello world!"))
+#should print Hlowrd
+#print(every_other_letter(""))
+#should print 
