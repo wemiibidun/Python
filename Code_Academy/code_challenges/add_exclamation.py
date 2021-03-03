ee60beb53dda99589ea4b9d4b63d2a72134c8d10
Wemimo Ibidunmoye <53876543+wemiibidun@users.noreply.github.com>
2021-03-03 06:22:12 -0700

    Update add_exclamation.py

--- Code_Academy/code_challenges/add_exclamation.py
+++ Code_Academy/code_challenges/add_exclamation.py
@@ -1,6 +1,8 @@
+'''
 Create a function named add_exclamation that has one parameter named word. 
 This function should add exclamation points to the end of word until word is 20 characters long. 
 If word is already at least 20 characters long, just return word.
+'''
 
 # Write your add_exclamation function here:
 def add_exclamation(word):
@@ -11,7 +13,7 @@ def add_exclamation(word):
 
 
 # Uncomment these function calls to test your function:
-print(add_exclamation("Codecademy"))
+#print(add_exclamation("Codecademy"))
 # should print Codecademy!!!!!!!!!!
-print(add_exclamation("Codecademy is the best place to learn"))
+#print(add_exclamation("Codecademy is the best place to learn"))
 # should print Codecademy is the best place to learn
