b1a90fd3ffd72ed5c9d5d8d54b8ad93cb1dbf1f9
Wemimo Ibidunmoye <53876543+wemiibidun@users.noreply.github.com>
2021-03-03 07:32:25 -0700

    Update count_x.py

--- practice_questions/count_x.py
+++ practice_questions/count_x.py
@@ -1,5 +1,7 @@
+'''
 Write a function named count_char_x that takes a string named word and a single character named x as parameters. 
 The function should return the number of times x appears in word.
+'''
 
 # Write your count_char_x function here:
 def count_char_x(word,x):
@@ -11,8 +13,8 @@ def count_char_x(word,x):
 
 
 
-# Uncomment these function calls to test your tip function:
-print(count_char_x("mississippi", "s"))
-# should print 4
-print(count_char_x("mississippi", "m"))
-# should print 1
+#Uncomment these function calls to test your tip function:
+#print(count_char_x("mississippi", "s"))
+#should print 4
+#print(count_char_x("mississippi", "m"))
+#should print 1
