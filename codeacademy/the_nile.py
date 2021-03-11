851556dd81c5861eb95ed1dbb69d9e0ab6bdd433
Wemimo Ibidunmoye <53876543+wemiibidun@users.noreply.github.com>
2021-03-11 06:55:12 -0700

    Create the_nile.py

---
+++ codeacademy/the_nile.py
@@ -0,0 +1,107 @@
+'''
+1. At The Nile our biggest concern is our consumers, and our biggest cost is delivering their goods to them. 
+We want to develop a program that will help us minimize these costs so we can continue making everyone happy.
+First we’ll need to calculate these costs using a function that you’re going to write called calculate_shipping_cost().
+Give calculate_shipping_cost three parameters: from_coords, to_coords, and shipping_type.
+
+2.Both from_coords and to_coords are tuples, containing first the latitude and then the longitude. 
+Since our get_distance() function looks for all four as separate arguments, we’ll need to separate these variables out.
+Inside calculate_shipping_cost unpack those tuples into from_lat, from_long, to_lat, and to_long.
+
+3.Now call get_distance(from_lat, from_long, to_lat, to_long) and save the result as distance.
+
+4. Next, get the shipping_rate by using the provided SHIPPING_PRICES dictionary and fetching the key passed in as shipping_type.
+
+5. Calculate the price by multiplying the distance by the shipping_rate
+
+6. Finally, return the formatted price, created by calling the provided format_price() on the price itself.
+
+7. What about our shoppers who hastily purchase goods without indicating their shipping type? Let’s give our function a default argument for shipping_type. 
+Since they’re in such a hurry let’s make the default argument 'Overnight'. 
+They’ll be happier to get what they ordered earlier, and we’ll be happier because they paid more money for it. It’s a win-win!
+
+8. Want to make sure you wrote the function correctly? Try calling test_function(calculate_shipping_cost) after your function definition.
+
+9. Write a function called calculate_driver_cost() with distance as the first parmameter, 
+and as many drivers as are available as positional arguments after that, as drivers.
+
+10. In order to find the best person, we need to calculate how much it would cost for any of the drivers to fulfill this order.
+Create two new variables, cheapest_driver and cheapest_driver_price. Set them both to None.
+
+11. Now let’s iterate over every driver in drivers. Use a for loop.
+
+12. First calculate the driver_time for each driver by multiplying driver.speed by distance.
+
+13. Next calculate the price_for_driver by multiplying driver.salary by driver_time
+
+14. Now we want to check if the current driver is the cheapest driver we’ve looked at.
+First, we’ll check if cheapest_driver is None, this likely means this is the first driver we’ve looked at.
+In that case, set cheapest_driver equal to driver and then set cheapest_driver_price equal to price_for_driver
+
+15. In an elif statment, check if price_for_driver is less than cheapest_driver_price. 
+This means that our current driver is cheaper than the driver stored in cheapest_driver.
+Update cheapest_driver to be equal to driver and update cheapest_driver_price to be equal to price_for_driver
+
+16. After outdenting out of our elif statement and the for loop, return cheapest_driver_price and cheapest_driver
+
+17. Let’s define a function called calculate_money_made(). This function will be passed a number of Trip IDs with corresponding trip information as arguments, 
+so let’s just take any keyword arguments passed into it. Store them all as trips!
+
+18. Let’s start a counter at 0. Create a variable called total_money_made that will count up for us
+
+19. Iterate through every trip_id and trip in the trips dictionary.
+
+20. Calculate the trip revenue into the variable trip_revenue by calculating trip.cost minus trip.driver.cost
+
+21. Add up that sweet revenue by incrementing total_money_made by trip_revenue
+
+22. Outside your for loop, return the total! Remember you can test your function by calling test_function(calculate_money_made) afterwards!
+'''
+
+from nile import get_distance, format_price, SHIPPING_PRICES
+from test import test_function #(import files not included)
+
+# Define calculate_shipping_cost() here:
+def calculate_shipping_cost(from_coords,to_coords,shipping_type = 'Overnight'):
+  from_lat,from_long = from_coords
+  to_lat, to_long = to_coords
+  distance =  get_distance(*from_coords, *to_coords) 
+  shipping_rate = SHIPPING_PRICES[shipping_type]
+  price = distance * shipping_rate
+  return format_price(price) 
+
+# Test the function by calling 
+test_function(calculate_shipping_cost)
+
+
+# Define calculate_driver_cost() here
+def calculate_driver_cost(distance, *drivers):
+  cheapest_driver = None
+  cheapest_driver_price = None
+
+  for driver in drivers:
+    driver_time = distance * driver.speed
+    price_for_driver = driver.salary * driver_time
+    
+    if cheapest_driver is None:
+      cheapest_driver = driver
+      cheapest_driver_price = price_for_driver
+    elif  price_for_driver <  cheapest_driver_price:
+      cheapest_driver = driver
+      cheapest_driver_price = price_for_driver
+
+  return cheapest_driver_price,cheapest_driver  
+
+# Test the function by calling 
+test_function(calculate_driver_cost)
+
+# Define calculate_money_made() here
+def calculate_money_made(**trips):
+  total_money_made = 0
+  for trip_id,trip in trips.items():
+    trip_revenue = trip.cost - trip.driver.cost
+    total_money_made += trip_revenue
+  return total_money_made  
+
+# Test the function by calling 
+test_function(calculate_money_made)
